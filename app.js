afeda99a516a3cc110bc22a383c836d90414b094
atw007wj <atw007wj@gmail.com>
2026-08-28 16:38:01 +0800

    Mirror line chart icon to be low left and high right

--- app.js
+++ app.js
@@ -858,7 +858,7 @@ function renderStats() {
         if (currentStatsChartType === 'line') {
             btnToggle.innerHTML = '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M18 20V10M12 20V4M6 20v-4"></path></svg>';
         } else {
-            btnToggle.innerHTML = '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>';
+            btnToggle.innerHTML = '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="2 12 6 12 9 21 15 3 18 12 22 12"></polyline></svg>';
         }
     }
     
@@ -1282,6 +1282,7 @@ if (statsViewEl) {
     });
 }
 
+
 
 
 
