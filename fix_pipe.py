8a1f15cdee63baf8a0c4954a7148762cd98f9ade
atw007wj <atw007wj@gmail.com>
2026-08-29 23:43:32 +0800

    Fix encoding issue for pipe symbol in log time rendering

---
+++ fix_pipe.py
@@ -0,0 +1,10 @@
+﻿import re
+
+with open('app.js', 'r', encoding='utf-8') as f:
+    js = f.read()
+
+# Replace the garbled character with the standard pipe symbol ' | '
+js = re.sub(r'Set \$\{entry\.setNumber\}[^$]+\$\{entry\.time\}', r'Set ${entry.setNumber} | ${entry.time}', js)
+
+with open('app.js', 'w', encoding='utf-8') as f:
+    f.write(js)
