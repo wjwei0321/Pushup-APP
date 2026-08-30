a1a6165d5ab680e567331740e8cb19f55267dd4f
atw007wj <atw007wj@gmail.com>
2026-08-30 15:16:07 +0800

    Reduce space in interval growth format

---
+++ fix_double_space.py
@@ -0,0 +1,10 @@
+﻿import re
+
+with open('app.js', 'r', encoding='utf-8') as f:
+    js = f.read()
+
+# Replace double space with single space
+js = js.replace("toLocaleString()}  ${signPct}", "toLocaleString()} ${signPct}")
+
+with open('app.js', 'w', encoding='utf-8') as f:
+    f.write(js)
---
+++ reduce_interval_space.py
@@ -0,0 +1,10 @@
+﻿import re
+
+with open('app.js', 'r', encoding='utf-8') as f:
+    js = f.read()
+
+# Replace ` &nbsp;` with ` `
+js = js.replace("&nbsp;${signPct}", " ${signPct}")
+
+with open('app.js', 'w', encoding='utf-8') as f:
+    f.write(js)
