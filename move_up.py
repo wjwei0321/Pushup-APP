13f460ce56e4ad9cbcdd6bf3d100821d234febbc
atw007wj <atw007wj@gmail.com>
2026-08-30 15:05:22 +0800

    Adjust margin and min-height of interval comparison to move it up

---
+++ move_up.py
@@ -0,0 +1,13 @@
+﻿import re
+
+with open('index.html', 'r', encoding='utf-8') as f:
+    html = f.read()
+
+# Modify statsIntervalComparison styling
+old_div = '<div id="statsIntervalComparison" style="min-height: 28px; display: flex; align-items: center;">'
+new_div = '<div id="statsIntervalComparison" style="min-height: 16px; display: flex; align-items: center; margin-top: -4px;">'
+
+html = html.replace(old_div, new_div)
+
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(html)
